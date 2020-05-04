69b9dccdae7893875bb1762d67b363a5b578f83c
olympia <owojcik7@gmail.com>
2020-05-04 10:44:35 -0400

    Refactor if/elses to checkRequired Function

--- script.js
+++ script.js
@@ -36,40 +36,66 @@ function isValidEmail(email) {
     return re.test(String(email).toLowerCase());
 }
 
-// Event listeners
+// Check required fields
+function checkRequired(inputArr) {
+    // Loop through array and do the check on each one
 
+    // Once we submit, we run checkRequired and console.log all inputs
+    inputArr.forEach(input => {
+        console.log(input.value)
+        // trim() trims whitespace
+        // input is the actual el, input el has an id property which gives us w/e the id is
+        if (input.value.trim() === '') {
+            console.log(input.id)
+            //template literal {input.id} vs getFieldName
+            showError(input, `${getFieldName(input)} is required`)
+        } else {
+            showSuccess(input)
+        }
+    })
+}
+
+// Get fieldname
+function getFieldName(input) {
+    // Get 1st letter, make uppercase, add rest of word
+    return input.id[0].toUpperCase() + input.id.slice(1)
+}
+
+
+// Event listeners
 form.addEventListener('submit', function (e) {
     e.preventDefault();
 
     console.log(username.value)
+    checkRequired([username, email, password, password2])
 
-    // Do this for every field
-    if (username.value === '') {
-        showError(username, 'Username is required')  //show red error message 
-    } else {
-        showSuccess(username) // show green outline for success
-    }
+    // // Do this for every field
+    // if (username.value === '') {
+    //     showError(username, 'Username is required')  //show red error message 
+    // } else {
+    //     showSuccess(username) // show green outline for success
+    // }
 
-    if (email.value === '') {
-        showError(email, 'Email is required')  //show red error message 
-    } else if (!isValidEmail(email.value)) {
-        showError(email, 'Email is not valid')
-    } else {
-        showSuccess(email) // show green outline for success
-    }
-
-
-    if (password.value === '') {
-        showError(password, 'Password is required')  //show red error message 
-    } else {
-        showSuccess(password) // show green outline for success
-    }
+    // if (email.value === '') {
+    //     showError(email, 'Email is required')  //show red error message 
+    // } else if (!isValidEmail(email.value)) {
+    //     showError(email, 'Email is not valid')
+    // } else {
+    //     showSuccess(email) // show green outline for success
+    // }
 
 
+    // if (password.value === '') {
+    //     showError(password, 'Password is required')  //show red error message 
+    // } else {
+    //     showSuccess(password) // show green outline for success
+    // }
 
-    if (password2.value === '') {
-        showError(password2, 'Confirm password is required')  //show red error message 
-    } else {
-        showSuccess(password2) // show green outline for success
-    }
+
+
+    // if (password2.value === '') {
+    //     showError(password2, 'Confirm password is required')  //show red error message 
+    // } else {
+    //     showSuccess(password2) // show green outline for success
+    // }
 })
