5a5f8888311a23c572d23f06f18a7adeb6529e04
olympia <owojcik7@gmail.com>
2020-05-04 18:58:35 -0400

    Check Length, Email, & Password Match

--- script.js
+++ script.js
@@ -31,9 +31,14 @@ function showSuccess(input) {
 // Check email is valid with regex
 // Matches an email address into a variable called re
 // Test method takes in w/e we pass in to see if it matches - if it does return True else return False
-function isValidEmail(email) {
+function checkEmail(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+
+    if (re.test(input.value.trim())) {
+        showSuccess(input)
+    } else {
+        showError(input, `Email is not valid`)
+    }
 }
 
 // Check required fields
@@ -55,6 +60,25 @@ function checkRequired(inputArr) {
     })
 }
 
+// Check input length
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`)
+    } else if (input.value.length > max) {
+
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`)
+    } else {
+        showSuccess(input)
+    }
+}
+
+// Check passwords match
+function checkPasswordsMatch(input1, input2) {
+    if (input1.value !== input2.value) {
+        showError(input2, `Passwords do not match`)
+    }
+}
+
 // Get fieldname
 function getFieldName(input) {
     // Get 1st letter, make uppercase, add rest of word
@@ -69,4 +93,10 @@ form.addEventListener('submit', function (e) {
     console.log(username.value)
     // Pass in an array of every field
     checkRequired([username, email, password, password2])
+
+    checkLength(username, 3, 15)
+    checkLength(password, 6, 25)
+    checkEmail(email)
+    checkPasswordsMatch(password, password2)
+
 })
